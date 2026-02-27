de4805a2458b8ab7d482852a27119ab4f7bdb221
alchemmist <anton.ingrish@gmail.com>
2026-02-27 18:08:34 +0300

    add support of code bock title bars

---
+++ uno.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "unocss";
+
+export default defineConfig({
+  safelist: [
+    "i-uil:github",
+    "i-tabler:brand-python",
+    "i-logos:ubuntu",
+    "i-logos:apple",
+    "i-logos:archlinux",
+    "i-streamline-logos:apple-logo-solid",
+    "i-devicon:windows8",
+    "i-mdi:bash",
+  ],
+});
